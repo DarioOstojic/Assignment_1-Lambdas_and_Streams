2520d4c225b2c09b2d8bbd86d8e81226859280a3
Dario Ostojic <darioostojic2@gmail.com>
2024-02-06 00:45:36 +0100

    Added additional parameters to the functional interface.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
         PersonGenerator personGenerator = new PersonGenerator(100);
         List<Person> persons = personGenerator.getPersons();
 
-        PersonFilter specifiedAgeFilter = personList -> personList.stream()
-                .filter(person -> person.getAge() < 30)
+        PersonFilter specifiedAgeFilter = (people, age, occupation, gender) -> people.stream()
+                .filter(person -> person.getAge() < age)
+                .filter(person -> person.getOccupation().equals(occupation))
+                .filter(person -> person.getGender().equals(gender))
                 .collect(Collectors.toList());
 
-        List<Person> filteredPersons = specifiedAgeFilter.filterPersons(persons);
+        List<Person> filteredPersons = specifiedAgeFilter.filterPersons(persons, 30, "Teacher", Gender.FEMALE);
 
         // Display the generated persons
         for (Person person : filteredPersons) {
--- src/PersonFilter.java
+++ src/PersonFilter.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 @FunctionalInterface
 public interface PersonFilter {
-    List<Person> filterPersons(List<Person> persons);
+    List<Person> filterPersons(List<Person> persons, int age, String occupation, Gender gender);
 
 }
